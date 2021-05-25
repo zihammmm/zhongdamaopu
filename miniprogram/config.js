a25d492b8a334b308d23bb76c003c4f7ffa25ee0
zihammmm <756177229@qq.com>
2021-05-26 00:42:21 +0800

    体验版0.1
    部署到云端，修改主色调和部分文案

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // 科普页图片
   science_imgs: [
-    "cloud://rel-eeeccf.7265-rel-eeeccf/系统/1.png",
-    "cloud://rel-eeeccf.7265-rel-eeeccf/系统/2.png",
-    "cloud://rel-eeeccf.7265-rel-eeeccf/系统/3.png",
-    "cloud://rel-eeeccf.7265-rel-eeeccf/系统/4.png",
-    "cloud://rel-eeeccf.7265-rel-eeeccf/系统/5.png"
+    "cloud://develop-5gth85z320dc2941.6465-develop-5gth85z320dc2941-1306036881/系统/科普1.png",
+    "cloud://develop-5gth85z320dc2941.6465-develop-5gth85z320dc2941-1306036881/系统/科普2.png",
+    "cloud://develop-5gth85z320dc2941.6465-develop-5gth85z320dc2941-1306036881/系统/科普3.png",
+    "cloud://develop-5gth85z320dc2941.6465-develop-5gth85z320dc2941-1306036881/系统/科普4.png",
+    "cloud://develop-5gth85z320dc2941.6465-develop-5gth85z320dc2941-1306036881/系统/科普5.png"
   ],
   // 赞赏码图片
-  reward_img: "cloud://rel-eeeccf.7265-rel-eeeccf-1258586139/系统/reward.jpg",
+  reward_img: "cloud://develop-5gth85z320dc2941.6465-develop-5gth85z320dc2941-1306036881/系统/赞赏码.jpg",
   // 新猫问卷图片
-  feedback_wj_img: "cloud://rel-eeeccf.7265-rel-eeeccf-1258586139/系统/新猫问卷.png",
+  feedback_wj_img: "cloud://develop-5gth85z320dc2941.6465-develop-5gth85z320dc2941-1306036881/系统/新猫问卷.png",
 }
